4011fac5e85c2d7b3af97b60e35c8cc1ba72621e
Eugene <eugene.yum@gmail.com>
2021-11-11 22:25:08 -0500

    adding initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# 11ty Starter
+
+Includes
+
+- 11ty
+- tailwindcss
+- tailwindcss typography
+
+## Fav Icons
+
+https://favicon.io/
+
+## Credits
+
+https://github.com/distantcam/windty
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [ "./src/_includes/**/*.njk", "./src/*.html" ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
